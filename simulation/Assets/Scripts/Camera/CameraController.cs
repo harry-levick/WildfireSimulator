0769afefd2e9b6c6a310cda231aae83ef4960e70
Hlev1 <harrylevick@hotmail.co.uk>
2020-11-20 10:52:27 +0000

    initial findings of fetching latitude and longitude

---
+++ simulation/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using Mapbox.Unity.Map;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float speed = 0.5f;
+    [SerializeField] float sensitivity = 0.1f;
+    [SerializeField] float distance = 1000f;
+    bool isMousePressed = false;
+
+    Camera cam;
+    AbstractMap map;
+    Vector3 anchorPoint;
+    Quaternion anchorRot;
+
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+        map = UnityEngine.Object.FindObjectOfType<AbstractMap>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 move = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+            move += Vector3.forward * speed;
+        if (Input.GetKey(KeyCode.S))
+            move -= Vector3.forward * speed;
+        if (Input.GetKey(KeyCode.D))
+            move += Vector3.right * speed;
+        if (Input.GetKey(KeyCode.A))
+            move -= Vector3.right * speed;
+        if (Input.GetKey(KeyCode.E))
+            move += Vector3.up * speed;
+        if (Input.GetKey(KeyCode.Q))
+            move -= Vector3.up * speed;
+        transform.Translate(move);
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            anchorPoint = new Vector3(Input.mousePosition.y, -Input.mousePosition.x);
+            anchorRot = transform.rotation;
+        }
+        if (Input.GetMouseButton(1))
+        {
+            Quaternion rot = anchorRot;
+            Vector3 dif = anchorPoint - new Vector3(Input.mousePosition.y, -Input.mousePosition.x);
+            rot.eulerAngles += dif * sensitivity;
+            transform.rotation = rot;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isMousePressed = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isMousePressed = false;
+        }
+
+        if (isMousePressed)
+        {
+            var mousePosScreen = Input.mousePosition;
+
+            mousePosScreen.z = Camera.main.transform.localPosition.y;
+            var pos = Camera.main.ScreenToWorldPoint(mousePosScreen);
+
+            var latlongDelta = map.WorldToGeoPosition(pos);
+            print($"Latitude: {latlongDelta.x} , Longitude: {latlongDelta.y}");
+        }
+    }
+}
+
