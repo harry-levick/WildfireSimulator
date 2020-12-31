c435c8336af4fe8c5aa86eb0361f9735136597b6
Hlev1 <harrylevick@hotmail.co.uk>
2020-12-31 12:47:35 +0000

    change units of wind speed
    metres per second to feet per minute

--- data/src/weather/app.py
+++ data/src/weather/app.py
@@ -3,8 +3,15 @@ from data.src.common.constants import *
 from flask import Flask, Response
 from flask import jsonify, request
 
+CURRENT = "current"
+HOURLY = "hourly"
+DAILY = "daily"
+WIND_SPEED = "wind_speed"
+WIND_DEGREES = "wind_deg"
 app = Flask(__name__)
 
+def metres_per_sec_to_feet_per_min(x: int) -> int:
+    return x * METRES_PER_SECOND_TO_FEET_PER_MINUTE_CONVERSION
 
 @app.route('/weather-data', methods=['GET'])
 def get_model_code() -> Response:
@@ -18,11 +25,6 @@ def get_model_code() -> Response:
         return Response(status=400)
 
 
-    current = "current"
-    hourly = "hourly"
-    daily = "daily"
-    wind_speed = "wind_speed"
-    wind_degrees = "wind_deg"
     url = WEATHER_API
 
     params = {
@@ -34,13 +36,18 @@ def get_model_code() -> Response:
 
     res = requests.get(url=url, params=params)
     data = res.json()
-    data[current] = {key: data[current][key] for key in data[current].keys() & {wind_speed, wind_degrees}}
+
+    data[CURRENT] = {key: data[CURRENT][key] for key in data[CURRENT].keys() & {WIND_SPEED, WIND_DEGREES}}
+    data[CURRENT][WIND_SPEED] = metres_per_sec_to_feet_per_min(data[CURRENT][WIND_SPEED])
 
     for i, dict in enumerate(data["hourly"]):
-        data[hourly][i] = {key: dict[key] for key in data[hourly][i].keys() & {wind_speed, wind_degrees}}
+        data[HOURLY][i] = {key: dict[key] for key in data[HOURLY][i].keys() & {WIND_SPEED, WIND_DEGREES}}
+        data[HOURLY][i][WIND_SPEED] = metres_per_sec_to_feet_per_min(data[HOURLY][i][WIND_SPEED])
 
     for i, dict in enumerate(data["daily"]):
-        data[daily][i] = {key: dict[key] for key in data[daily][i].keys() & {wind_speed, wind_degrees}}
+        data[DAILY][i] = {key: dict[key] for key in data[DAILY][i].keys() & {WIND_SPEED, WIND_DEGREES}}
+        data[DAILY][i][WIND_SPEED] = metres_per_sec_to_feet_per_min(data[DAILY][i][WIND_SPEED])
+
 
     return jsonify(data), 200
 
