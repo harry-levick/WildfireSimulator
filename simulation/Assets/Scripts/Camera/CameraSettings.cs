17863cae6f24fc1446ec8285b5d254426bfe3556
Hlev1 <harrylevick@hotmail.co.uk>
2021-01-19 13:19:39 +0000

    Revert "Revert "Create CameraSettings.cs""
    This reverts commit ddbfd8489a553ffbcb9dcaaa56c848ae35fe2e21.

---
+++ simulation/Assets/Scripts/Camera/CameraSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class CameraSettings
+{
+    [SerializeField]
+    public float Speed;
+
+    [SerializeField]
+    public float Sensitivity;
+
+    [SerializeField] public FireController FireController;
+
+
+    public CameraSettings()
+    {
+        Speed = 6.0f;
+        Sensitivity = 0.45f;
+        FireController = new FireController();
+    }
+}
