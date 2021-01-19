20633be54ae79917dfc4a42d16a1953ef2f5a4e7
Hlev1 <harrylevick@hotmail.co.uk>
2021-01-19 13:17:07 +0000

    Revert "refactoring in CameraScript and add CameraUISettings static class"
    This reverts commit 5415348bd181a812d0f396cc1ce46b3baf894132.

--- simulation/Assets/Scripts/Camera/CameraScript.cs
+++ simulation/Assets/Scripts/Camera/CameraScript.cs
@@ -30,48 +30,7 @@ public class CameraScript : MonoBehaviour
      * forces, torques, or other physics-related functions because you know 
      * it will be executed exactly in sync with the physics engine itself.
     */
-    private void FixedUpdate()
-    {
-        HandleMovement();
-    }
-
-
-    // Update is called once per frame
-    private void Update()
-    {
-        HandleLeftMouseButton();
-        HandleFireIgnition();
-    }
-
-
-
-    public void ToggleIgniting()
-    {
-        _cameraAction.ToggleIgniting();
-        IgniteButton.GetComponent<Image>().color =
-            CameraUISettings.GetIgnitingButtonColor(_cameraAction.GetIgniting());
-
-    }
-
-    private void HandleLeftMouseButton()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            _cameraAction.SetMousePressed(true);
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            _cameraAction.SetMousePressed(false);
-        }
-    }
-
-    private void HandleMovement()
-    {
-        HandleMovementKeys();
-        HandleRightMouseButton();
-    }
-
-    private void HandleMovementKeys()
+    void FixedUpdate()
     {
         Vector3 move = Vector3.zero;
         if (Input.GetKey(KeyCode.W))
@@ -87,10 +46,7 @@ public class CameraScript : MonoBehaviour
         if (Input.GetKey(KeyCode.Q))
             move -= Vector3.up * _settings.Speed;
         transform.Translate(move);
-    }
 
-    private void HandleRightMouseButton()
-    {
         if (Input.GetMouseButtonDown(1))
         {
             _anchorPoint = new Vector3(Input.mousePosition.y, -Input.mousePosition.x);
@@ -105,8 +61,19 @@ public class CameraScript : MonoBehaviour
         }
     }
 
-    private void HandleFireIgnition()
+
+    // Update is called once per frame
+    void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _cameraAction.SetMousePressed(true);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            _cameraAction.SetMousePressed(false);
+        }
+
         if (_cameraAction.IgniteFire())
         {
             if (EventSystem.current.IsPointerOverGameObject()) return;
@@ -117,6 +84,61 @@ public class CameraScript : MonoBehaviour
                 FireBehaviour newFire = _mapObject.AddComponent<FireBehaviour>();
                 newFire.Activate(hitInfo.point, ref _settings.FireController);
             }
+
+        }
+    }
+
+    float BearingBetweenInDegrees(Vector3 a, Vector3 b)
+    {
+        Vector3 normal = Vector3.up;
+        // angle in [0, 180]
+        float angle = Vector3.Angle(a, b);
+        float sign = Mathf.Sign(Vector3.Dot(normal, Vector3.Cross(a, b)));
+
+        // angle in [-179, 180]
+        float signedAngle = angle * sign;
+
+        // angle in [0, 360]
+        float bearing = (signedAngle + 360) % 360;
+        return bearing;
+    }
+
+    Vector2d GetLatLon(RaycastHit hitInfo)
+    {
+        return _map.WorldToGeoPosition(hitInfo.point);
+    }
+
+    float GetAltitudeInMeters(RaycastHit hitInfo)
+    {
+        return _map.QueryElevationInMetersAt(GetLatLon(hitInfo));
+    }
+
+    float GetSlopeInDegrees(RaycastHit hitInfo)
+    {
+        Vector3 normal = hitInfo.normal;
+        return Vector3.Angle(normal, Vector3.up);
+    }
+
+    float GetSlopeBearingInDegrees(RaycastHit hitInfo)
+    {
+        Vector3 normal = hitInfo.normal;
+
+        Vector3 left = Vector3.Cross(normal, Vector3.down);
+        Vector3 upslope = Vector3.Cross(normal, left);
+        Vector3 upslopeFlat = new Vector3(upslope.x, 0, upslope.z).normalized;
+
+        return BearingBetweenInDegrees(_north, upslopeFlat);
+    }
+
+    public void ToggleIgniting()
+    {
+        _cameraAction.ToggleIgniting();
+        if (_cameraAction.GetIgniting())
+        {
+            IgniteButton.GetComponent<Image>().color = Color.red;
+        } else
+        {
+            IgniteButton.GetComponent<Image>().color = Color.gray;
         }
     }
 
