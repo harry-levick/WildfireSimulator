91209cd3088d4e44c75da599d270d23ddd7e1cda
Hlev1 <harrylevick@hotmail.co.uk>
2021-01-20 20:59:27 +0000

    create UnityService

---
+++ simulation/Assets/Scripts/Services/IUnityService.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Services
+{
+    public interface IUnityService
+    {
+        public bool GetMouseButtonDown(int mouseButton);
+        public bool GetMouseButtonUp(int mouseButton);
+        public bool GetKey(KeyCode key);
+        public bool GetMouseButton(int mouseButton);
+    }
+}
---
+++ simulation/Assets/Scripts/Services/UnityService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Services
+{
+    public class UnityService : IUnityService
+    {
+        public bool GetKey(KeyCode key) => Input.GetKey(key);
+        public bool GetMouseButton(int mouseButton) => Input.GetMouseButton(mouseButton);
+        public bool GetMouseButtonDown(int mouseButton) => Input.GetMouseButtonDown(mouseButton);
+        public bool GetMouseButtonUp(int mouseButton) => Input.GetMouseButtonUp(mouseButton);
+    }
+}
