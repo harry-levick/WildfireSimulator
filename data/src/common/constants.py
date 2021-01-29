f99f65c6f76da35073d485038cb6d5d7126cfff1
Hlev1 <harrylevick@hotmail.co.uk>
2021-01-29 13:34:51 +0000

    Update constants.py

--- data/src/common/constants.py
+++ data/src/common/constants.py
@@ -4,6 +4,7 @@ FUEL_MODEL_FUEL_LOAD = "40_scott_and_burgan_fuel_models_fuel_load.csv"
 FUEL_MODEL_SAV_RATIO = "40_scott_and_burgan_fuel_models_sav_ratio.csv"
 
 FUEL_MOISTURE_MAP = "lfmc_map_2020-10-01_250.tif"
+#FUEL_MOISTURE_MAP = "lfmc_map_2020-07-15_250.tif"
 
 WGS84 = "EPSG:4326" # WGS84 latitude-longitude CRS
 
