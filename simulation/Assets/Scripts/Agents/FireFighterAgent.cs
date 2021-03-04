2c03c77aeee20f922676b4c4eb58ab335abef901
Hlev1 <harrylevick@hotmail.co.uk>
2021-03-04 20:53:31 +0000

    add control line creation by agent

--- simulation/Assets/Scripts/Agents/FireFighterAgent.cs
+++ simulation/Assets/Scripts/Agents/FireFighterAgent.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Fire;
 using Unity.MLAgents;
@@ -6,12 +7,20 @@ using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 using UnityEngine;
 using Random = UnityEngine.Random;
+using static Constants.StringConstants;
 
 namespace Agents
 {
     public class FireFighterAgent : Agent
     {
         public FireBehaviour fire;
+        private GameObject controlLine;
+        private List<GameObject> controlLines = new List<GameObject>();
+
+        public void Awake()
+        {
+            controlLine = Resources.Load(ControlLinePrefab) as GameObject;
+        }
 
         public override void OnEpisodeBegin()
         {
@@ -21,6 +30,9 @@ namespace Agents
             var localPosition = new Vector3(Random.Range(min, max), 500, Random.Range(min, max));
             transform.localPosition = localPosition;
 
+            controlLines.ForEach(line => line.Destroy());
+            controlLines.Clear();
+            
             fire.Reset();
             fire.Initialise(GetTerrainPoint());
         }
@@ -41,26 +53,41 @@ namespace Agents
             sensor.AddObservation(fire.weatherReport.current.wind_speed);
             sensor.AddObservation(fire.ignitionPoint);
         }
-        
+
         public override void OnActionReceived(ActionBuffers actions)
         {
             var moveX = actions.ContinuousActions[0];
             var moveZ = actions.ContinuousActions[1];
 
             const float moveSpeed = 400f;
-            
+            const float rotateDegrees = 90f;
+
             transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+
+            var placeControlLine = actions.DiscreteActions[0] == 1;
+            var rotate = actions.DiscreteActions[1] == 1;
+
+            if (!placeControlLine) return;
             
-            Debug.Log(actions.DiscreteActions[0]);
-            Debug.Log(actions.ContinuousActions[0]);
-            Debug.Log(actions.ContinuousActions[1]);
+            var holding = Instantiate(controlLine);
+            controlLines.Add(holding);
+            
+            holding.transform.position = GetTerrainPoint();
+
+            if (!rotate) return;
+
+            holding.transform.Rotate(Vector3.up, rotateDegrees);
         }
 
         public override void Heuristic(in ActionBuffers actionsOut)
         {
             ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+            ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
+            
             continuousActions[0] = Input.GetAxisRaw("Horizontal");
             continuousActions[1] = Input.GetAxisRaw("Vertical");
+
+            discreteActions[0] = Input.GetKey(KeyCode.Space) ? 1 : 0;
         }
 
         private void OnTriggerEnter(Collider other)
